8cebbb98b8932f773472396f14de92c74d245e36
JeffMunoz <Jamunozmorataya1134@eagle.fgcu.edu>
2019-09-16 22:22:02 -0400

    Added buttons to all the tabs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,3 +1,24 @@
 package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 // analyze-> inspect code
-public class Controller {}
+public class Controller {
+
+
+  @FXML
+  private void handleButtonAction(ActionEvent event) {
+    System.out.println("Hello World!");
+  }
+  @FXML
+  private void goodByeBTN(ActionEvent event) {
+    System.out.println("Good-Bye World!");
+  }
+
+  @FXML
+  private void welcomeBTN(ActionEvent event) {
+    System.out.println("General Kenobi!");
+  }
+}

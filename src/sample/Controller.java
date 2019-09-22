b952a658ca3666b5943ef67fce512ca85b2614d3
JeffMunoz <Jamunozmorataya1134@eagle.fgcu.edu>
2019-09-21 20:53:25 -0400

    Added labels to the tab pane and linked DB (not finished)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,38 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 // analyze-> inspect code
 public class Controller {
 
 
+
   @FXML
   private void handleButtonAction(ActionEvent event) {
-    System.out.println("Hello World!");
+
+    System.out.println("Product Added");
   }
   @FXML
   private void goodByeBTN(ActionEvent event) {
-    System.out.println("Good-Bye World!");
+    System.out.println("Production Recorded");
   }
 
   @FXML
   private void welcomeBTN(ActionEvent event) {
     System.out.println("General Kenobi!");
+
   }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Jeffry Munoz
@@ -30,5 +34,38 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+      // Some code obtained from tutorialsPoint https://www.tutorialspoint.com/h2_database/h2_database_jdbc_connection.htm
+      // JDBC driver name and database URL
+      final String JDBC_DRIVER = "org.h2.Driver";
+      final String DB_URL = "jdbc:h2:~/test";
+      final String USER = "";
+      final String PASS = "";
+
+
+      Connection conn = null;
+      Statement stmt = null;
+
+      try {
+        Class.forName(JDBC_DRIVER);
+
+        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+        stmt = conn.createStatement();
+        String sqlStmt = "INSERT INTO Product(type, manufacturer, name) VALUES ( 'Weapon', 'Tediore ', 'The Boo+' );";
+        stmt.execute(sqlStmt);
+
+        stmt.close();
+        conn.close();
+
+      } catch (ClassNotFoundException e) {
+        //e.printStackTrace();
+        System.out.println("Unable to find class");
+      }
+      catch(SQLException e) {
+        System.out.println("Error in SQL please try again");
+      }
+
+
     }
+
 }

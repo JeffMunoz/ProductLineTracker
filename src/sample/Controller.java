7fb7f973231c9a187188f7c87888358f5c5ac6c4
JeffMunoz <jamunozmorataya1134@eagle.fgcu.edu>
2019-09-23 19:20:23 -0400

    Moved code from main to the controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.collections.FXCollections;
@@ -15,14 +16,46 @@ import javafx.scene.control.TextField;
 
 // analyze-> inspect code
 public class Controller {
+  Statement stmt = null;
 
+  public void initialize() {
+    // Connection to the database
+    // Good place to start tutorialsPoint
+    // https://www.tutorialspoint.com/h2_database/h2_database_jdbc_connection.html
+    // JDBC driver name and database URL
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/ProductionDB";
+    final String USER = "";
+    final String PASS = "";
 
+    Connection conn = null;
+
+    try {
+      Class.forName(JDBC_DRIVER);
+
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      stmt = conn.createStatement();
+
+    } catch (ClassNotFoundException e) {
+      // e.printStackTrace();
+      System.out.println("Unable to find class");
+    } catch (SQLException e) {
+      System.out.println("Error in SQL please try again");
+    }
+  }
 
   @FXML
   private void handleButtonAction(ActionEvent event) {
-
-    System.out.println("Product Added");
+    try {
+      String sql = "SELECT * FROM PRODUCT ";
+      ResultSet rs = stmt.executeQuery(sql);
+      System.out.println("Product Added");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
+
   @FXML
   private void goodByeBTN(ActionEvent event) {
     System.out.println("Production Recorded");
@@ -31,8 +64,5 @@ public class Controller {
   @FXML
   private void welcomeBTN(ActionEvent event) {
     System.out.println("General Kenobi!");
-
   }
-
-
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.sql.ResultSet;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,7 +27,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Production Line");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -34,36 +35,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-      // Some code obtained from tutorialsPoint https://www.tutorialspoint.com/h2_database/h2_database_jdbc_connection.htm
-      // JDBC driver name and database URL
-      final String JDBC_DRIVER = "org.h2.Driver";
-      final String DB_URL = "jdbc:h2:~/test";
-      final String USER = "";
-      final String PASS = "";
-
-
-      Connection conn = null;
-      Statement stmt = null;
-
-      try {
-        Class.forName(JDBC_DRIVER);
-
-        conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-        stmt = conn.createStatement();
-        String sqlStmt = "INSERT INTO Product(type, manufacturer, name) VALUES ( 'Weapon', 'Tediore ', 'The Boo+' );";
-        stmt.execute(sqlStmt);
-
-        stmt.close();
-        conn.close();
-
-      } catch (ClassNotFoundException e) {
-        //e.printStackTrace();
-        System.out.println("Unable to find class");
-      }
-      catch(SQLException e) {
-        System.out.println("Error in SQL please try again");
-      }
 
 
     }
